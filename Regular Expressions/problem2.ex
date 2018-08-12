caba7843963571f3fb7d2c796008006da9f21093
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-08-12 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/problem2.ex
@@ -0,0 +1,3 @@
+#Problem 2: Write a program that matches email address.
+IO.puts Regex.match?(~r/^[_A-Za-z0-9-\+]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\.[A-Za-z0-9]+)*(\.[A-Za-z]{2,})$/, "mark@yahoo.com")
+IO.puts Regex.match?(~r/^[_A-Za-z0-9-\+]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\.[A-Za-z0-9]+)*(\.[A-Za-z]{2,})$/, "mark-yahoo.com")
