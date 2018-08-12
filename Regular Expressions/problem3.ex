811e4fbbc28f863b13b82090011b6860a93bca58
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-08-12 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/problem3.ex
@@ -0,0 +1,3 @@
+#Problem 3: Write a program that checks if string contains sample format date of (yyyy-mm-dd)
+IO.puts Regex.match?(~r/^([0-9]{4})-([0-9]{2})-([0-9]{2})/, "2018-01-02")
+IO.puts Regex.match?(~r/^([0-9]{4})-([0-9]{2})-([0-9]{2})/, "01-01-02")
