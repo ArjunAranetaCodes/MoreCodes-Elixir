9268a1b76fb1d86668dc44a5bdd4d6daf288f9f4
Arjun Araneta <arjunaraneta@gmail.com>
2018-08-14 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/problem14.ex
@@ -0,0 +1,3 @@
+#Problem 14: Write a program that recognizes valid hex color value using Regular Expression.
+IO.puts Regex.match?(~r/^([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])$/, "192.168.1.1")
+IO.puts Regex.match?(~r/^([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])$/, "1.1.1.1.1")
